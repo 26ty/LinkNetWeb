292af0ad4d71e7a841e4b380b7898be1a32194be
26ty <ch1342213422@gmail.com>
2023-08-03 12:11:41 +0800

    feat:新增文章卡

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //material component
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+//page
 import { HomeComponent } from './views/home/home.component';
 import { RecommendComponent } from './views/recommend/recommend.component';
-import { PrivyComponent } from './views/privy/privy.component'
+import { PrivyComponent } from './views/privy/privy.component';
+import { PrivyRoutingModule } from './views/privy/privy-routing.module'
+
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +28,14 @@ import { PrivyComponent } from './views/privy/privy.component'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    PrivyRoutingModule,
+    RouterModule.forRoot([
+      {path:"privy",component:PrivyComponent},
+      {path:"",component:HomeComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
