aec65cdd72d32d902933f3f69bb17453eec9c7dc
26ty <ch1342213422@gmail.com>
2023-08-03 16:53:20 +0800

    fix: 修改新增文章頁面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,16 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+//editor
+import { AngularEditorModule } from '@kolkov/angular-editor';
+
+//http
+import { HttpClientModule} from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
 
 //page
 import { HomeComponent } from './views/home/home.component';
@@ -45,7 +55,20 @@ import { EditArticleComponent } from './views/edit-article/edit-article.componen
     MatButtonModule,
     MatMenuModule,
     MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+
     
+    /* editor */
+    AngularEditorModule,
+
+    /* http */
+    HttpClientModule,
+
+    FormsModule,
+
+
+    /* routing */
     PrivyRoutingModule,
     RouterModule.forRoot([
       {path:"",component:HomeComponent},
--- src/app/views/add-article/add-article.component.ts
+++ src/app/views/add-article/add-article.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
-  selector: 'app-add-article',
+  selector: 'app-add-articletextarea[autoresize]',
   templateUrl: './add-article.component.html',
   styleUrls: ['./add-article.component.css']
 })
 export class AddArticleComponent {
+  constructor(){}
 
+  name = 'Angular 6';
+  htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '10rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text in this rich text editor....',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+    customClasses: [
+      {
+        name: 'Quote',
+        class: 'quoteClass',
+      },
+      {
+        name: 'Title Heading',
+        class: 'titleHead',
+        tag: 'h1',
+      },
+    ],
+  };
+
+  
 }
